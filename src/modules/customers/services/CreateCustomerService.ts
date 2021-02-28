ad0c388346c1d2999e98787cd81712cc261f367e
JúDCraide <juliadcraide31@gmail.com>
2021-02-28 16:01:52 -0300

    Customer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const usedEmail = await this.customersRepository.findByEmail(email);
+
+    if (usedEmail) {
+      throw new AppError('This email have already been used.');
+    }
+
+    const user = await this.customersRepository.create({ name, email });
+
+    return user;
   }
 }
 
